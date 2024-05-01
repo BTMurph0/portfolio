e2bd7d58f01c0a9c33163502e372b91ecdd348ba
Barry <barrymurphy_9@hotmail.com>
2024-05-01 14:53:43 +0100

    tech stack added

--- my-portfolio/src/pages/Dashboard.jsx
+++ my-portfolio/src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ export function Dashboard() {
       <Container mt="100px" maxWidth="960px">
         <Heading fontSize="3rem" color="#DBD8E3">Barry Murphy</Heading>
         <Heading fontSize="3rem" color="#DBD8E3" as="h3">Software Developer</Heading>
+        <Text fontSize="2rem" marginTop="20px" color="#DBD8E3">JavaScript || React || Node.js || Express.js || PostgreSQL</Text>
         <Text fontSize="2rem" marginTop="20px" color="#DBD8E3">Welcome to my portfolio site! Click below to see my projects.</Text>
         <ProjectsLink />
       </Container>
