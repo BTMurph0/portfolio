e75a2401e2b2abe2bf72a3fb4035ece39f471d76
Barry <barrymurphy_9@hotmail.com>
2024-05-07 11:06:33 +0100

    styling changes

--- my-portfolio/src/pages/Dashboard.jsx
+++ my-portfolio/src/pages/Dashboard.jsx
@@ -7,8 +7,8 @@ export function Dashboard() {
       <Container mt="100px" maxWidth="960px">
         <Heading fontSize="3rem" color="#DBD8E3">Barry Murphy</Heading>
         <Heading fontSize="3rem" color="#DBD8E3" as="h3">Software Developer</Heading>
-        <Text fontSize="2rem" marginTop="20px" color="#DBD8E3">JavaScript || React || Node.js || Express.js || PostgreSQL</Text>
-        <Text fontSize="2rem" marginTop="20px" color="#DBD8E3">Welcome to my portfolio site! Click below to see my projects.</Text>
+        <Text as="i" fontSize="2rem" marginTop="20px" color="#DBD8E3">JavaScript || React || Node.js || Express.js || PostgreSQL</Text>
+        <Text fontSize="2rem" marginTop="20px" color="#DBD8E3">Welcome to my portfolio site! I'm a Software Developer from Manchester with a decade of experience of the Software Development Lifecycle as a Software QA and Product Owner. Click below to see my projects.</Text>
         <ProjectsLink />
         <Box my="100px"></Box>
       </Container>
--- my-portfolio/src/pages/Projects.jsx
+++ my-portfolio/src/pages/Projects.jsx
@@ -5,26 +5,26 @@ import { DiNodejs, DiJsBadge } from "react-icons/di";
 export function Projects() {
   return (
     <Container mt="100px" maxWidth="960px">
-      <Heading mb="50px" color="mediumblue" fontSize="3rem">My Projects</Heading>
+      <Heading as="i" mb="50px" color="Purple" fontSize="3rem">My Projects</Heading>
       <Box>
         <ChakraLink href="https://resilient-alpaca-fd5bf3.netlify.app/" isExternal>
-        <Heading color="purple" fontSize="2rem" as="h3">NC News</Heading>
+        <Heading color="mediumblue" fontSize="2rem" as="h3">NC News</Heading>
         </ChakraLink>
         <Flex>
           <Text color="#DBD8E3">Tech stack:</Text>
-          <Icon color="#DBD8E3" w="50px" h="30px" as={BiLogoPostgresql} />
-          <Icon color="#DBD8E3" w="50px" h="30px" as={BiLogoReact} />
-          <Icon color="#DBD8E3" w="50px" h="30px" as={DiJsBadge} />
-          <Icon color="#DBD8E3" w="50px" h="50px" as={DiNodejs} />
+          <Icon color="turquoise" w="50px" h="30px" as={BiLogoPostgresql} />
+          <Icon color="turquoise" w="50px" h="30px" as={BiLogoReact} />
+          <Icon color="turquoise" w="50px" h="30px" as={DiJsBadge} />
+          <Icon color="turquoise" w="50px" h="50px" as={DiNodejs} />
         </Flex>
-        <Text color="#DBD8E3">
+        <Text as="i" color="#DBD8E3">
           This is my first project. It's a news website I made as a project on
           the Northcoders NC bootcamp. It was created using PostgreSQL and
           Express.js for the backend and React for the frontend. Users are
           greeted with a list of user profiles they can choose to login as. By
           default they are the guest profile. They cannot comment or vote on
           articles as a guest but selecting a user profile uses useContext to
-          unlock the comment and voting options.
+          unlock the comment and voting options. Click the project title above to see the hosted site.
         </Text>
       </Box>
     </Container>
